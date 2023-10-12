37f688747a465a88d104190e82da32e4514d4c4f
Vlad <sambipush@mail.ru>
2023-10-12 14:26:07 +0300

    Updates

---
+++ bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.title').addEventListener('click', () => {
+    console.log('Hello world!');
+  });
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
